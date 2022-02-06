4f3e4a3f66bb55a0214fb3280ebcea5bb780696f
Anneli Savchenko <95057484+Anneli-sf@users.noreply.github.com>
2022-02-06 22:37:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 module.exports = function towelSort (matrix) {
     
-        let arr=[];
-      for (i=0; i< matrix.length; i++) {
-        for (j=0; j< matrix[i].length; j++) {
-          arr.push(matrix[i][j]);
-        }
-      }
-    
+  let arr=[];
+  
+  if (matrix === undefined) { 
       return arr;
- 
+} else 
+  for (i=0; i< matrix.length; i++) {
+      for (j=0; j< matrix[i].length; j++) {
+        let row = i;
+        if (row % 2 === 0) 
+         row = j;
+         else row = (matrix[i].length-1-j); 
+          arr.push(matrix[i][row]);
+    }
+  }
+  return arr;
 }

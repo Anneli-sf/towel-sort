cf4d78d9f62ef342c93b244ac202f28a5de44818
Anneli Savchenko <95057484+Anneli-sf@users.noreply.github.com>
2022-07-21 15:08:54 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 module.exports = function towelSort (arr) {
     
-    arr.map(function(item, i) {
-  if (i%2 !== 0) item.sort((a,b) => b-a)
-})
-    
-return arr.flat();
+    let res = [];
+    if (arr === undefined) 
+        return res;
+    else 
+       arr.map(function(item, i) {
+        if (i % 2 !== 0) item.sort((a,b) => b-a);
+    });
+
+    return res = arr.flat();
     
 }
